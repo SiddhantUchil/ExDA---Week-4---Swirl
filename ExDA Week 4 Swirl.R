2b2f90e3292477d117633dfe4849d71ecc5e97cf
Siddhant Uchil <siddhant.uchil@somaiya.edu>
2020-06-14 13:06:34 +0530

    ExDA Week 4 Swirl Case Study

--- ExDA Week 4 Swirl.R
+++ ExDA Week 4 Swirl.R
@@ -51,20 +51,53 @@ cnt1 <- subset(pm1, State.Code == 36 & county.site %in% both)
 sapply(split(cnt0, cnt0$county.site), nrow)
 sapply(split(cnt1, cnt1$county.site), nrow)
 
+pm0sub <- with(pm0, County.Code == 63 & Site.ID == 2008) ##this is incorrect as it returns a logical string
 
+pm0sub <- subset(cnt0, County.Code == 63 & Site.ID == 2008)
+pm1sub <- subset(cnt1, County.Code == 63 & Site.ID == 2008)
 
+x0sub <- pm0sub$Sample.Value
+x1sub <- pm1sub$Sample.Value
 
+dates0 <- as.Date(as.character(pm0sub$Date), "%Y%m%d")
+dates1 <- as.Date(as.character(pm1sub$Date), "%Y%m%d")
 
+par(mfrow = c(1,2), mar = c(4,4,2,1))
 
+plot(dates0, x0sub, pch = 20)
+abline(h = median(x0sub, na.rm = TRUE), lwd = 2)
 
+plot(dates1, x1sub, pch = 20)
+abline(h = median(x1sub, na.rm = TRUE), lwd = 2)
 
+rng <- range(x0sub, x1sub, na.rm = TRUE)
+rng
 
+mn0 <- with(pm0, tapply(Sample.Value, State.Code, mean, na.rm = TRUE))
+str(mn0)
+mn1 <- with(pm1, tapply(Sample.Value, State.Code, mean, na.rm = TRUE))
+str(mn1)
 
+summary(mn0)
+summary(mn1)
 
+names(mn0)
+head(mn0)
+d0 <- data.frame(state = names(mn0), mean = mn0)
+d1 <- data.frame(state = names(mn1), mean = mn1)
 
+mrg <- merge(d0, d1, by = "state")
+dim(mrg)
+head(mrg)
 
+with(mrg, plot(rep(1, 52), mrg[ , 2], xlim = c(0.5, 2.5)))
+with(mrg, plot(rep(1, 52), mrg[ , 3]))
+points(rep(2, 52), mrg[ , 3])
+with(mrg, points(rep(2, 52), mrg[ , 3])) ##even without the with command the new points are plotted on the same graph
 
+segments(rep(1, 52), mrg[ , 2], rep(2, 52), mrg[ , 3])
 
+mrg[mrg$mean.x < mrg$mean.y, ]
 
 
 
